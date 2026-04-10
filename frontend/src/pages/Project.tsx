e7a240f18362c151cec20be02d0ddcc9a15c946b
ThomasRayDev <ilchenko290@gmail.com>
2026-04-10 17:34:02 +0300

    feat: верстка метрик на странице проекта

--- frontend/src/pages/Project.tsx
+++ frontend/src/pages/Project.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/ui/button';
 import { Progress } from '@/components/ui/progress';
-import { ArrowLeft, MapPin } from 'lucide-react';
+import { ArrowLeft, Calendar, ChartLine, DollarSign, MapPin, Users } from 'lucide-react';
 
 export default function Project() {
   return (
@@ -34,6 +34,48 @@ export default function Project() {
               <p className="text-primary font-semibold text-xl">85%</p>
             </div>
             <Progress className="h-3 bg-slate-700" value={85} />
+          </div>
+        </div>
+        <div className="mt-5 flex justify-between gap-5">
+          <div className="text-slate-100 w-full bg-[#0c1327] p-5 border-gray-800 border rounded-xl">
+            <div className="flex items-center gap-4">
+              <div className="bg-[#35d5c2]/10 w-max p-3 rounded-lg">
+                <DollarSign className="text-primary" />
+              </div>
+              <p className="text-slate-400 text-xs">Бюджет</p>
+            </div>
+            <p className="font-semibold text-2xl mt-4">1.2 млрд ₽</p>
+            <p className="text-slate-400 text-sm mt-1">Потрачено: 1.02 млрд ₽</p>
+          </div>
+          <div className="text-slate-100 w-full bg-[#0c1327] p-5 border-gray-800 border rounded-xl">
+            <div className="flex items-center gap-4">
+              <div className="bg-orange-400/10 w-max p-3 rounded-lg">
+                <Calendar className="text-orange-400" />
+              </div>
+              <p className="text-slate-400 text-xs">Срок сдачи</p>
+            </div>
+            <p className="font-semibold text-2xl mt-4">15 мар 2026</p>
+            <p className="text-slate-400 text-sm mt-1">Старт: 10 янв 2024</p>
+          </div>
+          <div className="text-slate-100 w-full bg-[#0c1327] p-5 border-gray-800 border rounded-xl">
+            <div className="flex items-center gap-4">
+              <div className="bg-blue-500/10 w-max p-3 rounded-lg">
+                <Users className="text-blue-500" />
+              </div>
+              <p className="text-slate-400 text-xs">Команда</p>
+            </div>
+            <p className="font-semibold text-2xl mt-4">45</p>
+            <p className="text-slate-400 text-sm mt-1">Специалистов</p>
+          </div>
+          <div className="text-slate-100 w-full bg-[#0c1327] p-5 border-gray-800 border rounded-xl">
+            <div className="flex items-center gap-4">
+              <div className="bg-green-500/10 w-max p-3 rounded-lg">
+                <ChartLine className="text-green-500" />
+              </div>
+              <p className="text-slate-400 text-xs">Выполнено</p>
+            </div>
+            <p className="font-semibold text-2xl mt-4">142</p>
+            <p className="text-slate-400 text-sm mt-1">Из 167 задач</p>
           </div>
         </div>
       </div>
